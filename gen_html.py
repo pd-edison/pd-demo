0068073e3fd88b44b9b77ee039f634f5ac32df09
pd-edison <pd-edison@ponddy-edu.net>
2021-03-18 13:27:20 -0700

    layout rearrange

--- gen_html.py
+++ gen_html.py
@@ -2,6 +2,7 @@ import os
 from glob import glob
 
 import yaml
+import numpy as np
 from jinja2 import FileSystemLoader, Environment
 
 
@@ -9,7 +10,9 @@ def gen_block_items():
     ret = []
     data_dirs = glob(os.path.join('data/*'))
     transcriptions = yaml.safe_load(open('data/transcriptions.yaml', 'r'))['data']
-
+    titles = []
+    gops = []
+    wavss = []
     for data_dir in data_dirs:
         if os.path.isfile(data_dir):
             continue
@@ -17,26 +20,28 @@ def gen_block_items():
         title = info.pop('title', os.path.basename(data_dir))
         gop = info.pop('gop', '-')
         wavs = sorted(glob(os.path.join(data_dir, '*.wav')))
-        item = (
-                title,
-                gop,
-                wavs,
-                transcriptions,
-            )
-        ret.append(item)
-    return ret
+        titles.append(title)
+        gops.append(gop)
+        wavss.append(wavs)
+    wavss = np.array(wavss).T
+    return titles, gops, transcriptions, wavss
 
 def main():
     """Main function."""
     loader = FileSystemLoader(searchpath="./templates")
     env = Environment(loader=loader)
-    env.globals.update(zip=zip)
-    template = env.get_template("base.html.jinja2")
+    env.globals.update(zip=zip, len=len)
+    template = env.get_template("pd.html.jinja2")
 
-    block_items = gen_block_items()
+    # block_items = gen_block_items()
+    titles, gops, transcriptions, wavss = gen_block_items()
 
     html = template.render(
-        block_items=block_items,
+        # block_items=block_items,
+        titles=titles,
+        gops=gops,
+        transcriptions=transcriptions,
+        wavss=wavss,
     )
     print(html)
 
