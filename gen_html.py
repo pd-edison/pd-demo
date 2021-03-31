bdf62ce045c33b0399a8b0fe7a054afc59e9cd15
pd-edison <pd-edison@ponddy-edu.net>
2021-03-30 23:51:56 -0700

    vocoder gop

--- gen_html.py
+++ gen_html.py
@@ -40,25 +40,38 @@ def gen_vocoder_items():
     ret = []
     info = yaml.safe_load(open('data/vocoder/info.yaml', 'r'))
     # data_dirs = glob(os.path.join('data/vocoder/*'))
+    vocoder_info = info.pop('vocoder_info')
     use_vocoders = info.pop('use_vocoders')
     use_files = info.pop('use_files')
-    data_dirs = [os.path.join('data/vocoder', use_vocoder) for use_vocoder in use_vocoders]
-    titles = ['raw']
-    wavs = [os.path.join('data/vocoder/raw', use_file+'.wav') for use_file in use_files]
-    wavss = [wavs]
+
+    data_dirs = [os.path.join('data/vocoder/', use_vocoder) for use_vocoder in use_vocoders]
+    titles = [] # ['raw']
+    wavs = [] # [os.path.join('data/vocoder/raw', use_file+'.wav') for use_file in use_files]
+    wavss = [] # [wavs]
+    gops = [] # ['-']
+    repos = [] # ['-']
+
     for data_dir in data_dirs:
         if os.path.isfile(data_dir):
             continue
         name = os.path.basename(data_dir)
         # wavs = sorted(glob(os.path.join(data_dir, '*.wav')))
+        _vocoder_info = vocoder_info.pop(name)
+        gop = _vocoder_info['gop']
+        repo = _vocoder_info['repo']
         wavs = [os.path.join(data_dir, use_file+'_gen.wav') for use_file in use_files]
         titles.append(name)
         wavss.append(wavs)
+        gops.append(gop)
+        repos.append(repo)
+        
     wavss = np.array(wavss, dtype=object).T
     meta = {
         'titles': titles,
         'use_files': use_files,
         'wavss': wavss,
+        'gops': gops,
+        'repos': repos,
     }
     return meta
 
